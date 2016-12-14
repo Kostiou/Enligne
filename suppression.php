39654dbc23454f36fbf01d1521e462fab405ea13
Kostiou <tcostiou@hotmail.com>
2016-12-14 11:51:59 +0100

    Add files via upload

--- suppression.php
+++ suppression.php
@@ -6,17 +6,17 @@ require_once 'settings/init.inc.php';       //permet l'affichage des erreurs
 if (isset($_GET['id'])) {   // vérifie l'existence d'un Id dans l'URL
     $id = $_GET['id'];  // déclaration de la variable id en récupérant l'id de l'URL
 
-    //Id présent donc on récupére l'article correspondant
-    $supprArt = $bdd->prepare("DELETE FROM articles WHERE id=$id"); //préparation de la requete pour récupération ID
+    //Suppression de l'article dont l'id est dans l'URL
+    $supprArt = $bdd->prepare("DELETE FROM articles WHERE id=$id"); //préparation de la requete de suppression
     $supprArt->execute(); //execution de la réquete;
     
-    
-    $supprCom =$bdd->prepare("DELETE FROM commentaires WHERE id_billet=$id"); //préparation de la requete pour récupération ID
+    //Suppression des commentaires correspondant à l'id_billet
+    $supprCom =$bdd->prepare("DELETE FROM commentaires WHERE id_billet=$id"); //préparation de la requete suppression
     $supprCom->execute(); //execution de la réquete;
     
     $_SESSION['suppr'] = TRUE;  //session validant la suppression
     
-    header("location: index.php");    //Recharge la page article en fin de processus
+    header("location: index.php");    //Recharge la page index en fin de processus
 } else {
-    header("location: index.php");    //Recharge la page article en fin de processus
+    header("location: index.php");    //Renvoi vers la page article
 }
